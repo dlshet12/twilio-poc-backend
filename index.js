432e9464f0b82075fe6a2a101450a6c68849988f
durga shetty <durgalaxmi.shetty@cognitiveclouds.com>
2023-10-23 20:03:12 +0530

    logged req

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function generateUniqueChatRoomName() {
 
 app.use(cors())
 app.post('/webhook', (req, res) => {
-  console.log('CALLED WEBHOOK HENCE MESSAGE SHOULD BE ADDED BRO!', JSON.stringify(req));
+  console.log('CALLED WEBHOOK HENCE MESSAGE SHOULD BE ADDED BRO!', req);
   res.sendStatus(200);
 })
 app.post('/twilio/token/:identity', async (req, res, next) => {
